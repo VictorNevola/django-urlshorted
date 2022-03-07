2949ed45332e652be75b06d01e679ad5a52b52c8
Victor Nevola <victornevolapn@gmail.com>
2022-03-06 21:34:44 -0300

    chore: add allowed host

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY_DJANGO")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://django-encurtador-url.herokuapp.com']
 
 
 # Application definition
